04874ae55ae5e028dc3ae1aaa9e140d2304af246
dimapartov <mr_kubarev@mail.ru>
2025-05-10 23:13:22 +0300

    Hotfix

--- src/main/java/org/example/gamerent/config/MvcConfig.java
+++ src/main/java/org/example/gamerent/config/MvcConfig.java
@@ -7,6 +7,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.io.File;
 
+
 @Configuration
 public class MvcConfig implements WebMvcConfigurer {
 
@@ -18,9 +19,8 @@ public class MvcConfig implements WebMvcConfigurer {
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         File uploadDir = new File(uploadPath);
         String absolutePath = uploadDir.getAbsolutePath();
-
         registry.addResourceHandler("/images/**")
-                .addResourceLocations("file:" + absolutePath + "/");
+                .addResourceLocations("file:" + absolutePath, "classpath:/static/images/");
     }
 
 }
